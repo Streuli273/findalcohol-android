2365ea0f4a25524a047d7dd2a61a972f5db1b973
Streuli273 <streuli273@gmail.com>
2015-01-25 19:20:47 +0000

    Begin to add location services

--- app/src/main/java/com/boa/findalcohol/MainActivity.java
+++ app/src/main/java/com/boa/findalcohol/MainActivity.java
@@ -1,10 +1,18 @@
 package com.boa.findalcohol;
 
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,16 +20,41 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+	private static final String TAG = "Find Alcohol!";
 
 	private GoogleMap map;
 
-	MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+	/** For GPlay Services */
+	protected GoogleApiClient mGoogleApiClient;
+
+	/** Stores parameters for requests to the FusedLocationProviderApi */
+	protected LocationRequest mLocationRequest;
+
+	/** Represents a geographical location */
+	protected Location mCurrentLocation;
+
+	/** For old location method (last location) */
+	protected Location mLastLocation;
+
+	/** Tracks the status of the location updates request */
+	protected Boolean mRequestingLocationUpdates;
+
+	/** Time when the location was updated represented as a String */
+	protected String mLastUpdateTime;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Set map
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
+
+		// Start process to get location
+		buildGoogleApiClient();
 	}
 
 
@@ -49,8 +82,91 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
 
 	@Override
 	public void onMapReady(GoogleMap map) {
-		map.addMarker(new MarkerOptions()
-		.position(new LatLng(53.378111798737095,-2.3410599914016075))
-		.title("Home"));
+		map.addMarker(new MarkerOptions().position(new LatLng(53.378, -2.341)).title("Home"));
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53.378, -2.341), 16));
+	}
+
+	protected synchronized void buildGoogleApiClient() {
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(LocationServices.API)
+				.build();
+	}
+
+	protected void createLocationRequest() {
+		LocationRequest mLocationRequest = new LocationRequest();
+		mLocationRequest.setInterval(10000);
+		mLocationRequest.setFastestInterval(5000);
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	}
+
+	private void startLocationUpdates() {
+		LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+	}
+
+	public void onLocationChanged(Location location) {
+		mCurrentLocation = location;
+		mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+		updateUI();
+	}
+
+	private void updateUI() {
+		// TODO Add UI interface
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+	}
+
+	/**
+	 * Runs when a GoogleApiClient object successfully connects.
+	 */
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		// Provides a simple way of getting a device's location and is well suited for
+		// applications that do not require a fine-grained location and that do not need location
+		// updates. Gets the best and most recent location currently available, which may be null
+		// in rare cases when a location is not available.
+
+		/** GET LOCATION */
+		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+		if (mLastLocation != null) {
+			Log.i(TAG, "Latitude = " + String.valueOf(mLastLocation.getLatitude()));
+			Log.i(TAG, "Longitude = " + String.valueOf(mLastLocation.getLongitude()));
+		} else {
+			Log.i(TAG, "No Location Available");
+		}
+
+		if (mRequestingLocationUpdates) {
+			startLocationUpdates();
+		}
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+		// onConnectionFailed.
+		Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+	}
+
+
+	@Override
+	public void onConnectionSuspended(int cause) {
+		// The connection to Google Play services was lost for some reason. We call connect() to
+		// attempt to re-establish the connection.
+		Log.i(TAG, "Connection suspended");
+		mGoogleApiClient.connect();
 	}
 }
